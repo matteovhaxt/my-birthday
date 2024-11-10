bf91061f11660360250a26821db814277270b6fe
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-11-10 20:44:46 +0100

    refactor: move code into src folder

--- backend/src/routes/invite/index.js
+++ backend/src/routes/invite/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../../data/database.js');
+const db = require('../../../data/database.js');
 
 router.get('/', (req, res) => {
   console.log('Fetching invites');
